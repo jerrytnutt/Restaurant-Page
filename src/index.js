9a2c040321a6cecd768192dc4f2fe13bb51a9ab3
Jerry <jerrynutt416@gmail.com>
2021-01-22 11:11:40 -0500

    change
    change

--- src/index.js
+++ src/index.js
@@ -10,24 +10,28 @@ const contactTab = document.querySelector('#contact')
 
 function clearDisplay(){
     const content = document.querySelector('#content')
+
     while (content.hasChildNodes()) {
         content.removeChild(content.lastChild);
     }
-
 }
+
 homeTab.addEventListener('click',() => {
 
     clearDisplay()
     return displayHome()
 });
+
 menuTab.addEventListener('click',() => {
 
     clearDisplay()
     return displayMenu()
 });
+
 contactTab.addEventListener('click',() => {
 
     clearDisplay()
     return displayContact()
 });
+
 displayHome()
